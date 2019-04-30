de58797bee8d2afc454335f6f4828704b4df3818
Jeroen Keesom <jeroen.keesom@exsyn.com>
2019-04-30 18:50:19 +0200

    menu button + Jquery
    Signed-off-by: Jeroen Keesom <jeroen.keesom@exsyn.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,23 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+window.$ = window.jQuery = require('jquery');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 require('materialize-css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+//const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+function initMaterializeElements() {
+    $(".dropdown-trigger:not(input)").dropdown({coverTrigger: false, constrainWidth: false});
+    $('.fixed-action-btn').floatingActionButton();
+    $('.tabs').tabs();
+    $('.modal').modal();
+    $('.tooltipped').tooltip();
+}
+
+$(document).ready(initMaterializeElements);
+
